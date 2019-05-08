a0b570ead3908291954183a30021f10b66ff7745
Siazon <296724771@qq.com>
2019-05-08 22:40:03 +0800

    33

--- TopWord/MainWindow.xaml.cs
+++ TopWord/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -26,44 +27,47 @@ namespace TopWord
     /// </summary>
     public partial class MainWindow : Window
     {
+        private NotifyIcon notifyIcon;
         public MainWindow()
         {
             InitializeComponent();
             this.Title = "topword";
+            this.ShowInTaskbar = false;
             this.Loaded += MainWindow_Loaded;
             Top();
+            ToolBarSetting();
         }
         public int Group = 10;
         public int time = 5;
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            string title = "";
-            Dictionary<string, int> dic = new Dictionary<string, int>();
-            List<tb_word> lista = SQLiteUtil.Ins.Query<tb_word>("select * from tb_word  ORDER BY seq");
-            foreach (var item in lista)
-            {
-                if (item.word[0].ToString().ToLower()=="a")
-                {
-                    title = "";
-                }
-                if (item.word.IndexOf('[')>=0)
-                title += " " + item.word.Substring(0,item.word.IndexOf('['));
-                else if (item.word.IndexOf('/') >= 0)
-                    title += " " + item.word.Substring(0, item.word.IndexOf('/'));
-                else if (item.word.IndexOf('-') >= 0)
-                    title += " " + item.word.Substring(0, item.word.IndexOf('-'));
-                else
-                    title += " " + item.word;
+            //string title = "";
+            //Dictionary<string, int> dic = new Dictionary<string, int>();
+            //List<tb_word> lista = SQLiteUtil.Ins.Query<tb_word>("select * from tb_word  ORDER BY seq");
+            //foreach (var item in lista)
+            //{
+            //    if (item.word[0].ToString().ToLower()=="a")
+            //    {
+            //        title = "";
+            //    }
+            //    if (item.word.IndexOf('[')>=0)
+            //    title += " " + item.word.Substring(0,item.word.IndexOf('['));
+            //    else if (item.word.IndexOf('/') >= 0)
+            //        title += " " + item.word.Substring(0, item.word.IndexOf('/'));
+            //    else if (item.word.IndexOf('-') >= 0)
+            //        title += " " + item.word.Substring(0, item.word.IndexOf('-'));
+            //    else
+            //        title += " " + item.word;
 
-            }
-            string tttt = title;
-            return;
+            //}
+            //string tttt = title;
+            //return;
             this.Topmost = true;
             List<tb_word> list = new List<tb_word>();
             Task.Run(() =>
             {
-                list = SQLiteUtil.Ins.Query<tb_word>("select * from tb_word where type=0");
+                list = SQLiteUtil.Ins.Query<tb_word>("select * from tb_word where type=0 ORDER BY seq,length(word)");
                 while (list.Count > 0)
                 {
                     List<tb_word> _currWord = list.GetRange(0, Group);
@@ -90,7 +94,64 @@ namespace TopWord
                 }
             });
         }
+        #region ToolBar
 
+        private void ToolBarSetting()
+        {
+            this.notifyIcon = new NotifyIcon();
+            this.notifyIcon.BalloonTipText = "系统运行中... ...";
+            this.notifyIcon.ShowBalloonTip(2000);
+            this.notifyIcon.Text = "系统运行中... ...";
+            this.notifyIcon.Icon = System.Drawing.Icon.ExtractAssociatedIcon(System.Windows.Forms.Application.ExecutablePath);
+            this.notifyIcon.Visible = true;
+            //打开菜单项
+            System.Windows.Forms.MenuItem open = new System.Windows.Forms.MenuItem("Open");
+            open.Click += new EventHandler(Show);
+            //打开菜单项
+            System.Windows.Forms.MenuItem hide = new System.Windows.Forms.MenuItem("Hide");
+            hide.Click += new EventHandler(Hide);
+            //退出菜单项
+            System.Windows.Forms.MenuItem exit = new System.Windows.Forms.MenuItem("Exit");
+            exit.Click += new EventHandler(Close);
+            //关联托盘控件
+            System.Windows.Forms.MenuItem[] childen = new System.Windows.Forms.MenuItem[] { open, hide, exit };
+            notifyIcon.ContextMenu = new System.Windows.Forms.ContextMenu(childen);
+
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler((o, e) =>
+            {
+                if (e.Button == MouseButtons.Left) this.Show(o, e);
+            });
+            //this.ShowInTaskbar = false;
+            //this.Visibility = System.Windows.Visibility.Hidden;
+
+        }
+
+        private void Show(object sender, EventArgs e)
+        {
+            this.Visibility = System.Windows.Visibility.Visible;
+            this.ShowInTaskbar = true;
+            this.Activate();
+            this.Show();
+        }
+
+        private void Hide(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = false;
+            this.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void Close(object sender, EventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+            return;
+            MessageBoxResult re = System.Windows.MessageBox.Show("关闭设备将会停运，您确定关闭吗？", "提示", MessageBoxButton.OKCancel);
+            if (re == MessageBoxResult.OK)
+            {
+                System.Windows.Application.Current.Shutdown();
+            }
+        }
+
+        #endregion
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             
