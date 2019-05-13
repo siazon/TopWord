5907738f0b656a0f1528a05fb8f91960b83387e1
siazon <296724771@qq.com>
2019-05-13 22:58:09 +0800

    layout

--- TopWord/ScreensaverView.xaml.cs
+++ TopWord/ScreensaverView.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,6 +12,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using TopWord.Dapper;
+using TopWord.Model;
 
 namespace TopWord
 {
@@ -19,15 +22,33 @@ namespace TopWord
     /// </summary>
     public partial class ScreensaverView : Window
     {
+        List<tb_word> list = new List<tb_word>();
         public ScreensaverView()
         {
             InitializeComponent();
             this.KeyDown += ScreensaverView_KeyDown;
+            list = SQLiteUtil.Ins.Query<tb_word>("select * from tb_word where type=0 limit 2000");
+            Task.Run(() =>
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    this.Dispatcher.Invoke(() =>
+                    {
+                        txtword.Text = list[i].word;
+                        txtphonetic.Text = "["+list[i].phonetic+"]";
+                        txtmeaning.Text = list[i].meaning;
+                    });
+
+                    Thread.Sleep(3000);
+                }
+
+            });
+
         }
 
         private void ScreensaverView_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.Key==Key.Escape)
+            if (e.Key == Key.Escape)
             {
                 this.Hide();
             }
